37d215ce242419681b57876044c8c75825aa73cf
Darko <Darko.Gelevski@corvus-info.com>
2019-04-17 16:25:22 +0200

    Ep-39 Done

---
+++ EmployeeManagment/Deserialize/Program.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+
+namespace Deserialize
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string json = @"{'Email': 'james@example.com','Active': true,'CreatedDate': '2013-01-20T00:00:00Z','Roles': ['User','Admin']}";
+            string json2 = @"{'Email':null,'Active': true,'CreatedDate': '2013-01-20T00:00:00Z','Roles': ['User','Admin']}";
+            var Account = JsonConvert.DeserializeObject<Account>(json2);
+            Console.WriteLine(Account.Email);
+            Console.Read();
+        }
+
+        public class Account
+        {
+            public Account()
+            {
+                Email = "";
+            }
+         
+            public string Email { get; set; }
+            public bool Active { get; set; }
+            public DateTime CreatedDate { get; set; }
+            public IList<string> Roles { get; set; }
+        }
+    }
+}

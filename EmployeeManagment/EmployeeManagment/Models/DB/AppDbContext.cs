fb718c9244790e003ebf02353f10bdf977bd4455
Darko <Darko.Gelevski@corvus-info.com>
2019-04-24 17:06:52 +0200

    progress

---
+++ EmployeeManagment/EmployeeManagment/Models/DB/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using EmployeeManagment.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace EmployeeManagement.Models.DB
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        DbSet<Employee> Employees { get; set; }
+    }
+}

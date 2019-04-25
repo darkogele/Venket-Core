ac616c208107e5e714acce0f4f5944bde69a218c
Darko <Darko.Gelevski@corvus-info.com>
2019-04-25 08:56:32 +0200

    sql and connectionstring added local vs

--- EmployeeManagment/EmployeeManagment/Startup.cs
+++ EmployeeManagment/EmployeeManagment/Startup.cs
@@ -10,15 +10,24 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.AspNetCore.Mvc;
+using EmployeeManagement.Models.DB;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace EmployeeManagment
 {
     public class Startup
     {
+        private IConfiguration _config;
+        public Startup(IConfiguration config)
+        {
+            _config = config;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services) //DI container
         {
+            services.AddDbContextPool<AppDbContext>(options => options.UseSqlServer(_config.GetConnectionString("EmployeeDBConnection")));
             services.AddSingleton<IEmployeeRepository, MockEmployeeRepository>();
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2).AddXmlSerializerFormatters();
         }
